5ed7966c1a7837da5fa94700ea035807d9096772
lexico4real <lexico4real@gmail.com>
2023-12-06 15:53:10 +0100

    initial setup

---
+++ src/main/java/com/oluyinka/droneapi/DroneApiApplication.java
@@ -0,0 +1,13 @@
+package com.oluyinka.droneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oluyinka/droneapi/DroneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oluyinka.droneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
